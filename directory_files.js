07ca5ab23565beb0a769158f81b9950cd509b708
Sabyasachi Das <sabyasachi23tatun.das@gmail.com>
2020-09-11 23:59:32 +0530

    Add fs module in directory_files.js

--- directory_files.js
+++ directory_files.js
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 const fs = require("fs");
 
+
+//READING DIRECTORIES
 // const files = fs.readdirSync("../Node_js");
 // console.log(files);
 
-
+//READING DIRECTORIES USING ASYNC 
 // fs.readdir("../Node_js", (err, files) =>{
 // 	if(err){
 // 		console.log(err);
@@ -14,27 +15,51 @@ const fs = require("fs");
 // });
 // console.log("Reading files");
 
+
 //READING FILE CONTENTS
-
-const contents = fs.readFileSync("./events.js", "UTF-8");
-=======
-const fs = require("fs");
-
-// const files = fs.readdirSync("../Node_js");
-// console.log(files);
+// const contents = fs.readFileSync("./events.js", "UTF-8");
+// console.log(contents);
 
 
-// fs.readdir("../Node_js", (err, files) =>{
+
+//WRITING FILES
+// const md = `
+// This is a new file.
+// This is written by filesystems module in Nodejs.
+// Uploaded by Sabyasachi Das.
+
+// `;
+// fs.writeFile("../Node_js/newFile.md", md, err =>{
 // 	if(err){
-// 		console.log(err);
+// 		throw(err);
 // 	} else{
-// 		console.log(files);
+// 		console.log("File Saved");
 // 	}
 // });
-// console.log("Reading files");
 
-//READING FILE CONTENTS
 
-const contents = fs.readFileSync("./events.js", "UTF-8");
->>>>>>> 15aef618956a08b557d90e6cc9d0bae5ed683b10
-console.log(contents);
+
+//CREATING A DIRECTORY
+// if(fs.existsSync("demoFiles")){
+// 	console.log("Directory already exists");
+// } else{
+// 	fs.mkdir("demoFiles", err =>{
+// 		if(err){
+// 			throw(err);
+// 		} else{
+// 			console.log("Directory created");
+// 		}
+// 	});
+// }
+
+//APPENDING A FILE
+const text = require("./assets/colors.json");
+text.colorList.forEach(c =>{
+	fs.appendFile("appendedFile.md", `${c.color}\n`, err =>{
+		if(err){
+			throw(err);
+		} else{
+			console.log("File Appended");
+		}
+	});
+});
